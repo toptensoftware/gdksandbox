05ca39d1454e560bcd5ae62424b446b7131fa711
Brad Robinson <contact@toptensoftware.com>
2020-06-04 17:16:58 +1000

    More hackery:

--- gtkcstest/MyWindow.cs
+++ gtkcstest/MyWindow.cs
@@ -61,21 +61,41 @@ namespace gtkcstest
         {
         }
 
+        static bool initial_configure_pending = true;
+        static int xTarget = 2469;
+        static int yTarget = 76;
+        static int xFrameDelta = 0;
+        static int yFrameDelta = 35;
+
         static Action_IntPtr base_realize;
         static unsafe void realize(IntPtr self)
         {
-            gtk_window_move(self, 2000, 100);
-            gtk_window_resize(self, 300, 300);
-            gtk_window_fullscreen(self);
+
+
+            //gtk_window_fullscreen(self);
+            Console.WriteLine(" -- WillRealize --");
             base_realize?.Invoke(self);
+            Console.WriteLine(" -- DidRealize --");
+
+
+            Console.WriteLine(" -- WillMove --");
+            gtk_window_move(self, xTarget - xFrameDelta, yTarget - yFrameDelta);
+            Console.WriteLine(" -- DidMove --");
+
+            Console.WriteLine(" -- WillSize --");
+            gtk_window_resize(self, 400, 300);
+            Console.WriteLine(" -- DidSize --");
+
+            gdk_window_get_origin(gtk_widget_get_window(self), out var x, out var y);
+            gdk_window_get_frame_extents(gtk_widget_get_window(self), out var rect);
         }
 
         static Action_IntPtr base_map;
         static unsafe void map(IntPtr self)
         {
+            Console.WriteLine(" -- WillMap --");
             base_map?.Invoke(self);
-            //gdk_window_move(gtk_widget_get_window(self), 500, 50);
-            //gdk_window_move_resize(gtk_widget_get_window(self), 1970, 320, 300, 300);
+            Console.WriteLine(" -- DidMap --");
         }
 
 
@@ -87,7 +107,7 @@ namespace gtkcstest
             unsafe
             {
                 GdkRectangle* pRect = (GdkRectangle*)allocation;
-                Console.WriteLine($"Allocate: {pRect->x},{pRect->y} x {pRect->width},{pRect->height}");
+                //Console.WriteLine($"Allocate: {pRect->x},{pRect->y} x {pRect->width},{pRect->height}");
             }
 
             /*
@@ -103,20 +123,51 @@ namespace gtkcstest
             unsafe
             {
                 GdkEventWindowState* pev = (GdkEventWindowState*)ev;
-                Console.WriteLine($"Window State: Maximized: {pev->new_window_state.HasFlag(GdkWindowState.GDK_WINDOW_STATE_MAXIMIZED)}");
+                //Console.WriteLine($"Window State: Maximized: {pev->new_window_state.HasFlag(GdkWindowState.GDK_WINDOW_STATE_MAXIMIZED)}");
             }
         }
 
         static Action_IntPtr_IntPtr_IntPtr base_configure_event;
         static void configure_event(IntPtr self, IntPtr ev, IntPtr data)
         {
+            base_configure_event?.Invoke(self, ev, data);
+
             unsafe
             {
                 var pev = (GdkEventConfigure*)ev;
-                Console.WriteLine($"Configure: {pev->x},{pev->y} x {pev->width},{pev->height}");
-            }
 
-            base_configure_event?.Invoke(self, ev, data);
+
+
+                Console.WriteLine($"---");
+                Console.WriteLine($"Configure: {pev->x},{pev->y} x {pev->width},{pev->height}");
+
+                gdk_window_get_root_origin(gtk_widget_get_window(self), out var xF, out var yF);
+                gdk_window_get_origin(gtk_widget_get_window(self), out var xC, out var yC);
+                gdk_window_get_frame_extents(gtk_widget_get_window(self), out var rect);
+                Console.WriteLine($"Extents      : {rect.x},{rect.y} x {rect.width},{rect.height}");
+                Console.WriteLine($"Origin       : {xC},{yC}");
+                Console.WriteLine($"Root Origin  : {xC},{yC}");
+
+                Console.WriteLine($"---");
+
+                if (initial_configure_pending && (pev->x != rect.x || pev->y != rect.y))
+                {
+                    int new_yFrameDelta = pev->y - rect.y;
+                    int new_xFrameDelta = pev->x - rect.x;
+
+                    if (new_yFrameDelta != yFrameDelta || new_xFrameDelta != xFrameDelta)
+                    {
+                        Console.WriteLine("Window needs moving...");
+                        gtk_window_move(self, rect.x - (new_xFrameDelta - xFrameDelta), rect.y - (new_yFrameDelta - yFrameDelta));
+                    }
+
+                    xFrameDelta = new_xFrameDelta;
+                    yFrameDelta = new_yFrameDelta;
+
+                    Console.WriteLine($"Frame adjustment updated to {xFrameDelta},{yFrameDelta}");
+                    initial_configure_pending = false;
+                }
+            }
         }
-}
+    }
 }
